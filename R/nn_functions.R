1b89cd52cbf7a160a6a31375f27cdb6f895daffb
Andrew McInerney <andrew.mcinerney@ul.ie>
2023-03-31 17:06:08 +0100

    fix bug nn_fit_tracks

--- R/nn_functions.R
+++ R/nn_functions.R
@@ -18,19 +18,21 @@
 nn_fit_tracks <- function(X, y, q, n_init, inf_crit = "BIC",
                           task = "regression", unif = 3, maxit = 1000, ...) {
   # Function with fits n_init tracks of model and finds best
-
+  
   df <- data.frame(X, y)
   n <- nrow(X)
   p <- ncol(as.matrix(X)) # as.matrix() in case p = 1 (auto. becomes vector)
-
+  
   k <- (p + 2) * q + 1
-
+  
+  colnames(df)[ncol(df)] <- "y"
+  
   weight_matrix_init <- matrix(stats::runif(n_init * k, min = -unif, max = unif), ncol = k)
-
+  
   weight_matrix <- matrix(rep(NA, n_init * k), ncol = k)
   inf_crit_vec <- rep(NA, n_init)
   converge <- rep(NA, n_init)
-
+  
   if (task == "regression") {
     linout <- TRUE
     entropy <- FALSE
@@ -43,39 +45,39 @@ nn_fit_tracks <- function(X, y, q, n_init, inf_crit = "BIC",
       task
     ))
   }
-
+  
   for (iter in 1:n_init) {
     nn_model <- nnet::nnet(y ~ .,
-      data = df, size = q, trace = FALSE,
-      linout = linout, entropy = entropy,
-      Wts = weight_matrix_init[iter, ], maxit = maxit, ...
+                           data = df, size = q, trace = FALSE,
+                           linout = linout, entropy = entropy,
+                           Wts = weight_matrix_init[iter, ], maxit = maxit, ...
     )
-
+    
     weight_matrix[iter, ] <- nn_model$wts
-
+    
     if (task == "regression") {
       RSS <- nn_model$value
       sigma2 <- RSS / n
-
+      
       log_likelihood <- (-n / 2) * log(2 * pi * sigma2) - RSS / (2 * sigma2)
     } else if (task == "classification") {
       log_likelihood <- -nn_model$value
     }
-
+    
     inf_crit_vec[iter] <- ifelse(inf_crit == "AIC",
-      (2 * (k + 1) - 2 * log_likelihood),
-      ifelse(inf_crit == "BIC",
-        (log(n) * (k + 1) - 2 * log_likelihood),
-        ifelse(inf_crit == "AICc",
-          (2 * (k + 1) * (n / (n - (k + 1) - 1)) - 2 * log_likelihood),
-          NA
-        )
-      )
+                                 (2 * (k + 1) - 2 * log_likelihood),
+                                 ifelse(inf_crit == "BIC",
+                                        (log(n) * (k + 1) - 2 * log_likelihood),
+                                        ifelse(inf_crit == "AICc",
+                                               (2 * (k + 1) * (n / (n - (k + 1) - 1)) - 2 * log_likelihood),
+                                               NA
+                                        )
+                                 )
     )
     converge[iter] <- nn_model$convergence
   }
   W_opt <- weight_matrix[which.min(inf_crit_vec), ]
-
+  
   return(list(
     "W_opt" = W_opt,
     "value" = min(inf_crit_vec),
